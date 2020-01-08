ceda1e9de08c26f3bf1595f6ad31bb7b0844efa4
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-08 16:06:52 +0100

    [test-suite] Add test for Expo token fetching

--- apps/test-suite/tests/NewNotifications.js
+++ apps/test-suite/tests/NewNotifications.js
@@ -37,6 +37,14 @@ export async function test(t) {
           t.expect(typeof devicePushToken.data).toBe('string');
           tokenFromMethodCall = devicePushToken;
         });
+
+        t.it('fetches Expo push token', async () => {
+          const devicePushToken = await Notifications.getExpoPushTokenAsync({
+            experienceId: '@exponent_ci_bot/test-suite',
+          });
+          t.expect(devicePushToken.type).toBe('expo');
+          t.expect(typeof devicePushToken.data).toBe('string');
+        });
       }
 
       if (Platform.OS === 'web') {
@@ -52,6 +60,14 @@ export async function test(t) {
         // but `t.pending()` still doesn't work.
         await waitFor(500);
         t.expect(tokenFromEvent).toEqual(tokenFromMethodCall);
+      });
+
+      t.it('fetches Expo push token', async () => {
+        const devicePushToken = await Notifications.getExpoPushTokenAsync({
+          experienceId: '@exponent_ci_bot/test-suite',
+        });
+        t.expect(devicePushToken.type).toBe('expo');
+        t.expect(typeof devicePushToken.data).toBe('string');
       });
     });
   });
